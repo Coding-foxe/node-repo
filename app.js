90f8d71e4094cf963b1e7195dd33ef4e5116c553
Coding-foxe <devpatrick100@gmail.com>
2023-02-14 15:58:01 +0100

    created the MVC folders

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-const home = 'home'
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+// middleware
+app.use(express.static('public'))
+
+// view engine
+app.set('view engine', 'ejs')
+
+// database connection
+const dbURI =
+	'mongodb+srv://Patrick:zarahkelo@nodeexpressproject.myxakyz.mongodb.net/JWT-tuts?retryWrites=true&w=majority'
+mongoose.set({ strictQuery: false })
+mongoose
+	.connect(dbURI)
+	.then(() =>
+		app.listen(3000, () => console.log('started server on port 3000'))
+	)
+	.catch(err => console.log(err))
+
+// routes
+app.get('/', (req, res) => res.render('home'))
+app.get('/smoothies', (req, res) => res.render('smoothies'))
