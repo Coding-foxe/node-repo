84932a608086e57a90e264f50d4ae239f59fcc1f
Coding-foxe <devpatrick100@gmail.com>
2023-03-01 21:44:46 +0100

    pushed the project

--- routes/authroute.js
+++ routes/authroute.js
@@ -8,17 +8,9 @@ const {
 
 const router = Router()
 
-router.get('/signup', () => {
-	signup_get()
-})
-router.post('/signup', () => {
-	signup_post()
-})
-router.get('/login', () => {
-	login_get()
-})
-router.post('/login', () => {
-	login_post()
-})
+router.get('/signup', signup_get)
+router.post('/signup', signup_post)
+router.get('/login', login_get)
+router.post('/login', login_post)
 
 module.exports = router
